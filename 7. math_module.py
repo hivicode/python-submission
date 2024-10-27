17acd301d9f7d8bcb79e98787e8fdc24bde9de5a
hivicode <bintangazzuri08@gmail.com>
2024-10-27 22:55:17 +0700

    changes

---
+++ 7. math_module.py
@@ -0,0 +1,6 @@
+import math
+a = float(input("input float number: "))
+print(math.ceil(a)) # literal ceil, you put 5.3 it'll print 6 at end.
+print(math.floor(a)) # literal floor, you put 5.3 it'll print 5 at end. 
+# reference https://docs.python.org/3/library/math.html
+# is WIP, akan saya lanjutkan lain waktu
