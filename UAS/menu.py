43705a6192339ee3d1d6932956cd393440f9c980
hivicode <bintangazuri08@gmail.com>
2025-01-26 13:42:04 +0700

    rename and minor changes to menu.py

--- UAS/menu.py
+++ UAS/menu.py
@@ -1,6 +1,6 @@
 from os import system
-system("cls")
-from maintes2 import transaksi
+system("clear")
+from main import transaksi
 ts = transaksi()
 
 while True:
@@ -22,39 +22,40 @@ while True:
             barang = str(input("Masukkan Nama Barang: "))
             harga = int(input("Masukkan Harga: "))
             jumlah = int(input("Masukkan Jumlah: "))
-            system("cls")
+            system("clear")
             ts.tambah_barang(barang, harga, jumlah)
             ts.lihat_keranjang()
         
         elif choice == 2:
             hapus_barang = str(input("Nama Barang Yang Ingin Dihapus: "))
-            system("cls")
+            system("clear")
             ts.hapus_barang(hapus_barang)
             ts.lihat_keranjang()
 
         elif choice == 3:
             ubah_nama = str(input("Nama Barang Yang Ingin Diubah: "))
             nama_baru = str(input("Masukkan Nama Baru: "))
-            system("cls")
+            system("clear")
             ts.ubah_namabarang(ubah_nama, nama_baru)
             ts.lihat_keranjang()
+            
         
         elif choice == 4:
             ubah_jumlah = str(input("Nama Barang Yang Ingin Diubah: "))
             jumlah_baru = int(input("Masukkan Jumlah Baru: "))
-            system("cls")
+            system("clear")
             ts.ubah_jumlahbarang(ubah_jumlah, jumlah_baru)
             ts.lihat_keranjang()
 
         elif choice == 5:
             ubah_harga = str(input("Nama Barang Yang Ingin Diubah: "))
             harga_baru = int(input("Masukkan Harga Baru: "))
-            system("cls")
+            system("clear")
             ts.ubah_harga(ubah_harga, harga_baru)
             ts.lihat_keranjang()
 
         elif choice == 6:
-            system("cls")
+            system("clear")
             if len(ts.keranjang) == 0:
                 print("\nKeranjang Kosong!\n")
             else:
@@ -65,7 +66,7 @@ while True:
             if hapus_keranjang.upper() == "N":
                 print("Batal Menghapus")
             elif hapus_keranjang.upper() == "Y":
-                system("cls")
+                system("clear")
                 ts.hapus_semua()
             else:
                 print("Mohon Pilih y atau n!")
@@ -73,7 +74,7 @@ while True:
         elif choice == 8:
             ts.total_jumlah()
             bayar_tunai = int(input("Masukkan Jumlah Tunai Anda: "))
-            system("cls")
+            system("clear")
             ts.header()
             ts.lihat_keranjang()
             ts.diskon()
@@ -81,7 +82,8 @@ while True:
             ts.bayar(bayar_tunai)
             belanja_lagi = str(input("Ingin Belanja Lagi? (y/n): "))
             if belanja_lagi.upper() == "Y":
-                system("cls")
+                system("clear")
+                ts.hapus_semua()
                 continue
             else:
                 belanja_lagi.upper() == "N"
@@ -90,16 +92,16 @@ while True:
         elif choice == 9:
             keluar = str(input("Yakin? (y/n): "))
             if keluar.upper() == "Y":
-                system("cls")
+                system("clear")
                 break
             else:
                 keluar.upper() == "N"
                 continue
 
         else:
-            system("cls")
+            system("clear")
             print("\nPilihan Nomor Tidak Valid\n")
 
     except ValueError:
-        system("cls")
+        system("clear")
         print("\nMohon Masukkan Yang Benar\n")
