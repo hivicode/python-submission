66864ca7e76f704d38a63a9878895cefc8171262
b <bintangazzuri08@gmail.com>
2025-03-06 13:09:57 +0700

    Create pt2_casting.py

---
+++ pt2_casting.py
@@ -0,0 +1,17 @@
+# TIPE DATA (studi kasus sendiri)
+# Nama: Bintang Fathir F.A
+# NIM: 1124102166
+
+# studi kasus: mengubah tipe data dari harga barang
+
+#input harga barang dengan tipe data string
+harga = input("Harga Barang: ")
+
+#variable tipe data
+int_harga = int(harga) #adding *integer* to convert
+float_harga = float(harga) #addding *float* to convert
+
+#output tipe data yang telah diubah
+print (harga, type(harga))
+print (int_harga, type(int_harga))
+print (float_harga, type(float_harga))
