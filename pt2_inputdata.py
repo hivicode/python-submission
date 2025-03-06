010282cf3f70ced7a0104254eabaacda3f20b45b
b <bintangazzuri08@gmail.com>
2025-03-06 13:05:03 +0700

    Create pt2_inputdata.py

---
+++ pt2_inputdata.py
@@ -0,0 +1,14 @@
+# TIPE DATA
+# Nama: Bintang Fathir F.A
+# NIM: 1124102166
+
+# studi kasus: membuat input biodata, setelah input selesai membersihkan terminal dan menampilkan inputannya dengan detail yang lebih rapi
+
+from os import system
+nama = input("Nama anda: ")
+pt = input("Nama Perguruan Tinggi: ")
+tahun_masuk = int(input("Tahun Masuk: "))
+nim = int(input("NIM: "))
+prodi = input("Program Studi: ")
+system ("cls")
+print ("Halo, nama saya", nama+".", "\nSaya adalah mahasiswa", pt, "Angkatan", tahun_masuk, "\nDengan NIM", nim, "\nSaya mengambil Program Studi", prodi+".")
