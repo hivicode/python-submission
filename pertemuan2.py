77a6fea2b63cc6bc95675d88682a202fb85311fe
b <bintangazzuri08@gmail.com>
2025-03-06 12:53:22 +0700

    Create pertemuan2.py

---
+++ pertemuan2.py
@@ -0,0 +1,19 @@
+# VARIABEL
+# Nama: Bintang Fathir F.A
+# NIM: 1124102166
+
+# studi kasus: membuat banyak operasi aritmatika namun operasinya menggunakan variable
+
+#variable angka1 dan angka2 untuk menginput angka yang ingin dioperasikan
+angka1 = int(input("masukan angka pertama: "))
+angka2 = int(input("masukan angka kedua: "))
+
+#variable operasi
+tambah = angka1 + angka2
+kurang = angka1 - angka2
+bagi = angka1 / angka2
+
+#output hasil
+print(f"{angka1} ditambah {angka2} = {tambah}")
+print(f"{angka1} dikurangi {angka2} = {kurang}")
+print(f"{angka1} dibagi {angka2} = {bagi}")
