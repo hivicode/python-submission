1703aa316f91a99f1f073772a0b159a747bd95e2
b <bintangazzuri08@gmail.com>
2025-03-06 13:19:16 +0700

    Update pt2_casting.py

--- SP2.1 Praktikum Algoritma dan Pemrograman/pt2_casting.py
+++ SP2.1 Praktikum Algoritma dan Pemrograman/pt2_casting.py
@@ -11,7 +11,7 @@ harga = input("Harga Barang: ")
 int_harga = int(harga) #adding *integer* to convert
 float_harga = float(harga) #addding *float* to convert
 
-#output tipe data yang telah diubah
+#output tipe data yang telah diinput beserta yang sudah diubah
 print (harga, type(harga))
 print (int_harga, type(int_harga))
 print (float_harga, type(float_harga))
