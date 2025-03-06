e051b412d81ae6e2cb3c1f6def7082a136adecc0
b <bintangazzuri08@gmail.com>
2025-03-06 13:01:46 +0700

    Create pt2_tipedata.py

---
+++ pt2_tipedata.py
@@ -0,0 +1,39 @@
+# TIPE DATA
+# Nama: Bintang Fathir F.A
+# NIM: 1124102166
+
+# studi kasus: membuat banyak variable dengan berbagai macam tipe data lalu menampilkan tipe datanya
+
+#variable
+var_1 = True
+var_2 = "hello sekai"
+var_3 = 66
+var_4 = 6.66
+var_5 = complex(1j)
+var_6 = list(("kucing", "landak", "tupai"))
+var_7 = tuple(("kucing", "landak", "tupai"))
+var_8 = range(6)
+var_9 = dict(nama="Doe", umur=37)
+var_10 = set(("kucing", "landak", "tupai"))
+var_11 = frozenset(("kucing", "landak", "tupai"))
+var_12 = bytes(5)
+var_13 = bytearray(5)
+var_14 = memoryview(bytes(5))
+
+#output tipedata
+print(("tipedata: "), type(var_1))
+print(("tipedata: "), type(var_2))
+print(("tipedata: "), type(var_3))
+print(("tipedata: "), type(var_4))
+print(("tipedata: "), type(var_5))
+print(("tipedata: "), type(var_6))
+print(("tipedata: "), type(var_7))
+print(("tipedata: "), type(var_8))
+print(("tipedata: "), type(var_9))
+print(("tipedata: "), type(var_10))
+print(("tipedata: "), type(var_11))
+print(("tipedata: "), type(var_12))
+print(("tipedata: "), type(var_13))
+print(("tipedata: "), type(var_14))
+
+      
