a8cb2e879386817d4f1091f1e956e4027ee6b1e4
b <bintangazzuri08@gmail.com>
2025-03-20 13:09:14 +0700

    Update tugas_praktikum3.py

--- SP2.1 Praktikum Algoritma dan Pemrograman/pertemuan 4/tugas_praktikum3.py
+++ SP2.1 Praktikum Algoritma dan Pemrograman/pertemuan 4/tugas_praktikum3.py
@@ -1,1 +1,29 @@
+# Pertemuan 4 (studi kasus sendiri)
+# Nama: Bintang Fathir F.A
+# NIM: 1124102166
 
+# Studi kasus: pendeteksi spesies
+# Syarat:
+
+# IF ELSE ✔
+# ELIF ✔
+# NESTED IF ✔
+
+import random
+print("\nProgram Pendeteksi Spesies")
+print("-"*20)
+nama = input("Nama Anda: ")
+presentase = random.randint(0, 101)
+
+if presentase >= 50 :
+    spesies = "sedikit reptil"
+    if presentase >= 99 :
+        spesies = "sangat reptil"
+    else:
+        spesies = "cukup reptil"
+elif presentase >= 25 :
+    spesies = "sedikit lagi jadi reptil" 
+else:
+    spesies = "manusia"
+
+print(f"Spesies: {presentase}% Anda {spesies}\n")
