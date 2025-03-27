7b2a0ce877dce4b10c571ce4ea7b43194affb486
b <bintangazzuri08@gmail.com>
2025-03-27 12:42:52 +0700

    Add files via upload

---
+++ SP2.1 Praktikum Algoritma dan Pemrograman/pertemuan 5/while loop_tugaspraktikum 4.py
@@ -0,0 +1,33 @@
+# Studi kasus: membuat perulangan dari angka awal 0 sampai kurang dari atau sama dengan 10 dengan kelipatan 2
+
+print("While loop")
+awal = 0 #nilai awal 0
+while awal <= 10: # jika nilai awal kurang dari atau sama dengan 10, maka
+    print(awal) # tampilkan
+    awal += 2 # dengan kondisi berkelipatan 2
+
+print("\n")
+
+# Studi kasus: membuat perulangan dari angka awal 0 sampai kurang dari atau sama dengan 16 dengan kelipatan 2,
+# namun berhentikan tampilan saat di angka 10
+
+print("While if loop")
+awal = 0
+while awal <= 16:
+    print(awal)
+    if awal == 10: # jika nilai awal mencapai perulangan angka 10, maka
+        break # berhentikan
+    awal += 2
+
+print("\n")
+
+# Studi kasus: membuat perulangan dari angka awal 0 sampai kurang dari 10 dengan kelipatan 2,
+# dan tampilkan bahwa angka awal tidak lebih kurang dari 10
+
+print("While else loop")
+awal = 0
+while awal < 10:
+  print(awal)
+  awal += 2
+else:
+  print("nilai tidak lebih kurang dari 10")
