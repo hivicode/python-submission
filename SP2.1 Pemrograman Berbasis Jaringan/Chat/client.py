2fc69f617f8c82ace3ac91ceef9d2d47fbca20d2
b <bintangazzuri08@gmail.com>
2025-04-24 08:45:48 +0700

    Update client.py

--- SP2.1 Pemrograman Berbasis Jaringan/Chat/client.py
+++ SP2.1 Pemrograman Berbasis Jaringan/Chat/client.py
@@ -1,7 +1,7 @@
 import socket
 from threading import Thread
 
-SERVER_HOST = "192.168.137.158"
+SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5002
 separator_token = "<SEP>"
 name = input("Masukkan nama anda: ")
