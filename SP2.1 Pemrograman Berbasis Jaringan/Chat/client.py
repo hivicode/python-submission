962ad1c575f8b0a4c6ef28c435ce68dc66a4eb42
b <bintangazzuri08@gmail.com>
2025-04-24 08:52:50 +0700

    Update client.py

--- SP2.1 Pemrograman Berbasis Jaringan/Chat/client.py
+++ SP2.1 Pemrograman Berbasis Jaringan/Chat/client.py
@@ -1,24 +1,24 @@
 import socket
 from threading import Thread
 
-SERVER_HOST = "0.0.0.0"
+SERVER_HOST = "192.168.137.158"
 SERVER_PORT = 5002
 separator_token = "<SEP>"
-name = input("Masukkan nama anda: ")
+
+name = input("Masukkan nama kamu: ")
 
 s = socket.socket()
-print(f"[*] Menghubungkan ke {SERVER_HOST}:{SERVER_PORT}...")
+print(f"[+] Menghubungkan ke {SERVER_HOST}:{SERVER_PORT}...")
 s.connect((SERVER_HOST, SERVER_PORT))
 print("[+] Terhubung.")
 
 def listen_for_messages():
     while True:
         try:
-            msg = s.recv(1024).decode()
-            print("\n" + msg)
+            message = s.recv(1024).decode()
+            print("\n" + message)
         except Exception as e:
-            print("[!] Koneksi terputus:", e)
-            s.close()
+            print("[!] Terputus dari server.")
             break
 
 t = Thread(target=listen_for_messages)
@@ -26,13 +26,10 @@ t.daemon = True
 t.start()
 
 while True:
-    to_send = input()
-    if to_send.lower() == "exit":
+    msg = input()
+    if msg.lower() == "exit":
         break
-    message = f"{name}{separator_token}{to_send}"
-    try:
-        s.send(message.encode())
-    except Exception as e:
-        print("[!] Error mengirim pesan: ", e)
-        break
+    full_msg = f"{name}{separator_token}{msg}"
+    s.send(full_msg.encode())
+
 s.close()
